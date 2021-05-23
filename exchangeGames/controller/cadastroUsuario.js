d752c11b229a3abcdf8b823aa997ae3363f75e36
Paulo Daniel <pda.ambrosio@gmail.com>
2021-05-23 20:50:03 -0300

    registro de usuario adicionado

--- exchangeGames/controller/cadastroUsuario.js
+++ exchangeGames/controller/cadastroUsuario.js
@@ -4,6 +4,52 @@ const bcrypt = require('bcrypt');
 
 let cadastroJson = path.join('modules/usuarios.json');
 
+function registrar (usuario) {
+    const gravar = JSON.stringify(usuario);
+    fs.writeFileSync(cadastroJson, gravar);
+};
+
+function lerRegistro () {
+    const ler = fs.readFileSync(cadastroJson);
+    const dados = JSON.parse(ler);
+    return dados
+};
+
+function buscarRegistro (email) {
+    const usuarios = lerRegistro();
+    return usuarios.find(usuario => usuario.email === email);
+}
+
+async function encriptarSenha(senha) {
+    const salt = await bcrypt.genSalt(5)
+    return await bcrypt.hash(senha, salt)
+};
+
 module.exports.cadastro = (req, res) => {
-    res.render('cadastroUsuario');
+    res.render('cadastroUsuario', {
+        mensagem: ''
+    });
+};
+
+module.exports.registrarUsuario = async (req, res) => {
+    const usuario = req.body;
+
+    if (buscarRegistro(usuario.email)) {
+        res.render('cadastroUsuario', {
+            mensagem: 'Usuário já cadastrado.'
+        });
+    } else {
+        const hash = await encriptarSenha(usuario.senha);
+        const usuarios = lerRegistro();
+        usuarios.push({
+            nome: usuario.nome,
+            sobrenome: usuario.sobrenome,
+            nome_usuario: usuario.nome_usuario,
+            email: usuario.email,
+            hash
+        });
+        registrar(usuarios);
+    }
+
+    res.redirect('/admin/login');
 };
--- exchangeGames/routes/cadastro.js
+++ exchangeGames/routes/cadastro.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const cadastroController = require('../controller/cadastroUsuario');
 
 router.get('/', cadastroController.cadastro);
+router.post('/', cadastroController.registrarUsuario);
 
 module.exports = router
