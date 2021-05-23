2693850c26f0c4d80584c51dfff1246d831badba
Paulo Daniel <pda.ambrosio@gmail.com>
2021-05-23 15:10:26 -0300

    router corrigida para /cadastro

--- exchangeGames/routes/cadastro.js
+++ exchangeGames/routes/cadastro.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 const cadastroController = require('../controller/cadastroUsuario');
 
-router.get('/cadastroUsuario', cadastroController.cadastro);
+router.get('/', cadastroController.cadastro);
 
 module.exports = router
