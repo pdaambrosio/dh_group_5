a1394589aad39403eeef346f147c06e4a9121250
Maria Rita Aragão <mia.aragao.a@gmail.com>
2021-05-24 17:59:38 -0300

    pagina de perguntas frequentes atualizada

--- exchangeGames/controller/home.js
+++ exchangeGames/controller/home.js
@@ -1,3 +1,10 @@
 module.exports.quem = function(req, res) {
     res.render('quem-somos');
   }
+
+  module.exports.perguntas = function(req, res) {
+    res.render('perguntasFrequentes');
+  }
+
+  
+
--- exchangeGames/routes/home.js
+++ exchangeGames/routes/home.js
@@ -4,6 +4,8 @@ const homeController = require('../controller/home')
 
 router.get('/quem-somos', homeController.quem);
 
+router.get('/perguntas', homeController.perguntas);
+
 
   module.exports = router
 
