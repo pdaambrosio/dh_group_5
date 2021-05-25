1ccce2128ecdf5e5e2ef0a276da749f5c17941a7
matheus <math_bms@hotmail.com>
2021-05-25 14:02:50 -0300

    adiciona login no css

---
+++ exchangeGames/controller/usersController.js
@@ -0,0 +1,3 @@
+module.exports.login = (req, res) => {
+    res.send(req.body);
+  }
--- exchangeGames/routes/users.js
+++ exchangeGames/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controller/usersController')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/login', userController.login)
 
 module.exports = router;
