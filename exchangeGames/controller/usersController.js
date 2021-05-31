84f253b0e78c8fb5f2b61df1db86c27e5bf97ca3
matheus <math_bms@hotmail.com>
2021-05-31 19:10:10 -0300

    simplifica login

--- exchangeGames/controller/usersController.js
+++ exchangeGames/controller/usersController.js
@@ -1,13 +1,37 @@
 const fs = require('fs')
 const path = require('path')
+const bcrypt = require('bcrypt')
 const { lerCadastro } = require('../helpers/lerCadastro');
 
-module.exports.buscarCadastro = function (email) {
+buscarCadastro = function (email) {
   const usuario = lerCadastro();
   return usuario.find(usuarios => usuarios.email === email);
 }
 
 module.exports.suaConta = (req, res) => {
   res.render('suaConta');
+}
+
+module.exports.fazLogin = async (req, res) => {
+  const login = req.body
+  const usuarios = buscarCadastro(login.email)
+
+  if (!usuarios) {
+    res.send(404)
+  } else {
+    if (await validarSenha(login.senha, usuarios.hash)) {
+      res.redirect('/users/suaConta')
+    } else {
+      res.send(400)
+    }
+  }
+}
+
+module.exports.paginaLogin = (req, res, next) => {
+  res.render('login');
+}
+
+async function validarSenha(senha, hash) {
+  return await bcrypt.compare(senha, hash)
 }
 
--- exchangeGames/routes/users.js
+++ exchangeGames/routes/users.js
@@ -1,32 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt')
 const usersController = require('../controller/usersController')
 
 router.get('/suaConta', usersController.suaConta);
 
-router.get('/login', function(req, res, next) {
-  res.render('login');
-});
+router.get('/login', usersController.paginaLogin);
 
-router.post('/login', async (req, res) => {
-  const login = req.body
-  const usuarios = usersController.buscarCadastro(login.email)
-
-  if (!usuarios) {
-    res.send(404)
-  } else {
-    if (await validarSenha(login.senha, usuarios.hash)) {
-      res.redirect('/users/suaConta')
-    } else {
-      res.send(400)
-    }
-  }
-})
-
-async function validarSenha(senha, hash) {
-  return await bcrypt.compare(senha, hash)
-}
+router.post('/login', usersController.fazLogin);
 
 
 module.exports = router;
+
+
+
+
+
+
+
