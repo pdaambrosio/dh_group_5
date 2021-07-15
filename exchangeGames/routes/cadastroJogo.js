f4408c26197151fe82fbbeaa5891500c2b0313c3
em4nnuelle <emannuelle.bs@gmail.com>
2021-07-15 19:03:45 -0300

    multer e css cadastro jogo

--- exchangeGames/routes/cadastroJogo.js
+++ exchangeGames/routes/cadastroJogo.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const uuid = require('uuid').v4
+
+const uploads= []
 
 /*Importando o multer*/
 const multer = require('multer')
+
+/*const upload = multer({
+    dest: 'uploads/'
+})*/
 
 const storage = multer.diskStorage({
     destination: (req, files, cb) => {
         const formatosAceitos = ['image/jpeg', 'image/png']
         
         if (formatosAceitos.includes(files.mimetype)) {
-            cb(null, path.join(__dirname,'../uploads'))
+            cb(null, path.join(__dirname,'../public/uploads'))
         } else {
             cb('Formatos aceitos: .JPG e .PNG')
         }
     },
-    filename: (req, file, cb) => {
-        cb(null,file.originalname)
+    filename: (req, files, cb) => {
+        cb(null, `${uuid()}_${files.originalname}`)
     }
 })
 
@@ -41,8 +48,17 @@ router.post('/upload', upload.single('foto'), (req, res) => {
 
 router.post('/multiupload', upload.array('fotos'), (req, res) => {
     console.log(req.files)
+    for(let file of req.files){
+    uploads.push(file.filename)
+    }
+    console.log(req.body)
     res.send(204)
 })
 
+/*Rota detalhes teste*/
+router.get('/teste-detalhes', (req, res) => {
+    res.render('detalhesTeste', { images:uploads })
+})
 
 module.exports = router  
+ 
