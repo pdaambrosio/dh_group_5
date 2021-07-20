b0583afecb396f9726cceb2acf532ed8e9ff3e63
Paulo Daniel <pda.ambrosio@gmail.com>
2021-07-20 18:18:26 -0300

    cadastrar model alterado

--- exchangeGames/helpers/cadastrar.js
+++ exchangeGames/helpers/cadastrar.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const cadastroJson = path.join('modules/usuarios.json');
+const cadastroJson = path.join('model/usuarios.json');
 
 module.exports.cadastrar = function (usuario) {
     const gravar = JSON.stringify(usuario);
