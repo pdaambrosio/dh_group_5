e1dc456044c44ac72ceb930a6346bd67ecd4b1c3
Paulo Daniel <pda.ambrosio@gmail.com>
2021-07-26 16:36:10 -0300

    alterado apontamento para o controller loginUsuario

--- exchangeGames/routes/users.js
+++ exchangeGames/routes/users.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const loginUsuario = require('../controller/loginUsuario')
 
 router.get('/suaConta', loginUsuario.suaConta);
-
 router.get('/login', loginUsuario.paginaLogin);
-
 router.post('/login', loginUsuario.fazLogin);
 
 module.exports = router; 
