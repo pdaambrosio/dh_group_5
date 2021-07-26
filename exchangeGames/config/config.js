c733a90da1b5693160f783b2bf5965dc053b4eee
Paulo Daniel <pda.ambrosio@gmail.com>
2021-07-26 18:13:48 -0300

    config.js original para pr

--- exchangeGames/config/config.js
+++ exchangeGames/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 module.exports ={
   "development": {
-    "username": "root",
-    "password": "mysql",
-    "database": "exchange",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
@@ -23,4 +23,3 @@ module.exports ={
     "dialect": "mysql"
   }
 }
-  
