fbffac2e7082bff975e9ddec4a7fd7a2bc371c35
em4nnuelle <emannuelle.bs@gmail.com>
2021-08-01 23:13:54 -0300

    validação nome do jogo

--- exchangeGames/controller/cadastroJogoController.js
+++ exchangeGames/controller/cadastroJogoController.js
@@ -13,6 +13,12 @@ function salvandoJogo(arrayDeJogos) {
 module.exports.cadastrandoJogo = async (req, res) => {
   const generos = [].concat(req.body.genero)
   console.log(req.body)
+  if(req.body.nomeJogo > 150){
+      const erro = "Nome maior que 150 caracteres!"
+      const genero = await db.Genero.findAll()
+      const plataforma = await db.Plataforma.findAll()
+      return res.render('cadastroDeJogo', {genero, plataforma, erro})
+  }
   const novoJogo = await db.Anuncio.create({
   ano: req.body.anoJogo,
   descricao: req.body.descricao,
@@ -29,15 +35,18 @@ module.exports.cadastrandoJogo = async (req, res) => {
     generos_id: generos[i]
     })
   }
+
+
   res.redirect('/jogos')
 }
 
 module.exports.cadastroJogo = async (req, res) => {
+  const erro = ""
   const genero = await db.Genero.findAll()
   const plataforma = await db.Plataforma.findAll()
-  console.log(genero[0].toJSON())
-  console.log(plataforma[0].toJSON())
-  res.render('cadastroDeJogo', {genero, plataforma})
+  //console.log(genero[0].toJSON())
+  //console.log(plataforma[0].toJSON())
+  res.render('cadastroDeJogo', {genero, plataforma, erro})
 }
 
 
