6e6f267d07c9a817583f957661ba9cf893fd1da5
em4nnuelle <emannuelle.bs@gmail.com>
2021-08-16 20:27:20 -0300

    req session no cadastro jogo

--- exchangeGames/controller/cadastroJogoController.js
+++ exchangeGames/controller/cadastroJogoController.js
@@ -13,49 +13,49 @@ module.exports.cadastrandoJogo = async (req, res) => {
   if(!req.body.nomeJogo){
     const erro = "Insira um nome para o jogo."
     const retorno = await buscarGeneroPlataforma()
-    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro})
+    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro, usuarioLogado: req.session.nickname})
   }
   if(req.body.nomeJogo.length > 150){
     //console.log(req.body.nomeJogo + " " + req.body.nomeJogo.length)
       const erro = "Atenção: Nome maior que 150 caracteres."
       const retorno = await buscarGeneroPlataforma()
-      return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro})
+      return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro, usuarioLogado: req.session.nickname})
   }
   if(!req.body.anoJogo){
     const erro = "Atenção: Insira o ano do jogo."
     const retorno = await buscarGeneroPlataforma()
-    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro})
+    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro, usuarioLogado: req.session.nickname})
   }
   if(req.body.anoJogo.length != 4){
       const erro = "Atenção: Formato do ano aceito: AAAA"
       const retorno = await buscarGeneroPlataforma()
-      return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro})
+      return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro, usuarioLogado: req.session.nickname})
   }
   if(!req.body.descricao){
     const erro = "Atenção: Insira uma descrição para o anúncio."
     const retorno = await buscarGeneroPlataforma()
-    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro})
+    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro, usuarioLogado: req.session.nickname})
   }
   if(req.body.descricao.length > 1500){
     const erro = "Atenção: Descrição maior que 1500 caracteres."
     const retorno = await buscarGeneroPlataforma()
-    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro})
+    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro, usuarioLogado: req.session.nickname})
   }
   if(!req.body.tempoJogo){
     const erro = "Atenção: Insira o tempo de uso."
     const retorno = await buscarGeneroPlataforma()
-    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro})
+    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro, usuarioLogado: req.session.nickname})
   }
   if(req.body.tempoJogo.length > 25){
     const erro = "Atenção: Tempo de uso maior que 25 caracteres."
     const retorno = await buscarGeneroPlataforma()
-    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro})
+    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro, usuarioLogado: req.session.nickname})
   }
     //console.log(req.files[0])
   if(!req.files[0].filename){
     const erro = "Atenção: Envie ao menos uma foto do seu jogo."
     const retorno = await buscarGeneroPlataforma()
-    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro})
+    return res.render('cadastroDeJogo', {genero:retorno[0], plataforma:retorno[1], erro, usuarioLogado: req.session.nickname})
   }
   
   const novoAnuncio = await db.Anuncio.create({
@@ -90,7 +90,7 @@ module.exports.cadastroJogo = async (req, res) => {
   const plataforma = await db.Plataforma.findAll()
   //console.log(genero[0].toJSON())
   //console.log(plataforma[0].toJSON())
-  res.render('cadastroDeJogo', {genero, plataforma, erro})
+  res.render('cadastroDeJogo', {genero, plataforma, erro, usuarioLogado: req.session.nickname})
 }
 
 
