ebc1c8f78b24e667757ab7de75d4f80a93204e25
em4nnuelle <emannuelle.bs@gmail.com>
2021-08-22 22:48:01 -0300

    valida sessao no cadastro jogo

--- exchangeGames/routes/cadastroJogo.js
+++ exchangeGames/routes/cadastroJogo.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../middlewares/multer')
+const validaSessao = require ('../middlewares/validaSessao');
 
 const CadastroJogoController = require('../controller/cadastroJogoController')
 
-router.get('/', CadastroJogoController.cadastroJogo)
+router.get('/', validaSessao, CadastroJogoController.cadastroJogo)
 
-router.post('/cadastroJogo', upload.array('fotos'), CadastroJogoController.cadastrandoJogo)
+router.post('/cadastroJogo', validaSessao, upload.array('fotos'), CadastroJogoController.cadastrandoJogo)
 
-router.get('/editarJogo/:id', upload.array('fotos'), CadastroJogoController.editarJogo)
-router.post('/editarJogo/:id', upload.array('fotos'), CadastroJogoController.editandoJogo)
+router.get('/editarJogo/:id', validaSessao, upload.array('fotos'), CadastroJogoController.editarJogo)
+router.post('/editarJogo/:id', validaSessao, upload.array('fotos'), CadastroJogoController.editandoJogo)
 
 module.exports = router  
